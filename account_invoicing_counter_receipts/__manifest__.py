70eddbb872f8b8e6dd4feb86f64090064a573218
EL Abquina <eabquina@gmail.com>
2024-11-30 19:44:25 +0800

    Counter Receipt Module

---
+++ account_invoicing_counter_receipts/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Counter Receipts",
+    'summary': "Counter Receipts for Customer Invoices or Vendor Bills",
+    'description': """
+        Counter Receipts for Customer Invoices or Vendor Bills
+    """,
+    'author': "Tech Ops PH",
+    'website': "https://techops.ph",
+    'category': 'Invoicing',
+    'version': '17.0',
+    'depends': ['account_invoicing_manual_fields'],
+
+    # always loaded
+    'data': [
+        'data/ir_actions_report.xml',
+        'data/ir_sequence_data.xml',
+        'security/ir.model.access.csv',
+        'reports/counter_receipts.xml',
+        'views/counter_receipts.xml',
+        'views/account_move.xml',
+        'views/menu_actions.xml',
+    ],
+}
+
