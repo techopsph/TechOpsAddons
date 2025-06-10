14378f29455cd9bda73432723efc59b51c236117
EL Abquina <eabquina@gmail.com>
2025-06-10 17:45:46 +0800

    refactor to compute URL now

--- website_slides_custom_video/models/slide_slide.py
+++ website_slides_custom_video/models/slide_slide.py
@@ -13,37 +13,27 @@ class Slide(models.Model):
     video_source_type = fields.Selection(
         selection_add=[('custom', 'Custom')],
         ondelete={'custom': 'set null'},
+        compute="_compute_video_source_type",
     )
-
-    embed_code_custom = fields.Html('Custom Embed Code', sanitize=False)
     
-    
-    @api.onchange('custom_video_source')
-    def _onchange_custom_video_source(self):
+    @api.depends('video_url', 'custom_video_source')
+    def _compute_video_source_type(self):
+        """Determine the video source type based on the video URL."""
+        super()._compute_video_source_type()  # Call the original method to preserve existing logic
         for slide in self:
-            if slide.custom_video_source:
+            if slide.video_source_type != 'custom' and slide.custom_video_source:
                 slide.video_source_type = 'custom'
-            else:
-                slide.video_source_type = False
 
-    @api.onchange('video_source_type', 'video_url', 'custom_video_source')
-    def _onchange_embed_code_custom(self):
-        for slide in self:
-            if (
-                slide.video_source_type == 'custom'
-                and slide.video_url
-                and not slide.embed_code_custom
-            ):
-                slide.embed_code_custom = (
-                    f'<iframe src="{slide.video_url}" '
-                    'frameborder="0" allowfullscreen></iframe>'
-                )
-    
-    @api.depends('slide_category', 'video_source_type', 'embed_code_custom', 'custom_video_source')
+    @api.depends('slide_category', 'video_source_type', 'custom_video_source')
     def _compute_embed_code(self):
         """Compute the embed code based on custom video source."""
         super()._compute_embed_code()  # Call the original method to preserve existing logic
         for slide in self:
-            if slide.slide_category == 'video' and slide.video_source_type == 'custom' and slide.embed_code_custom:
-                slide.embed_code = Markup(slide.embed_code_custom)
-                slide.embed_code_external = slide.embed_code_custom
+            embed_code = False
+            embed_code_external = False
+            if slide.video_source_type == 'custom':
+                video_url = Markup('<iframe src="%s" class="o_wslides_iframe_viewer" allowFullScreen="true" height="%s" width="%s" frameborder="0" aria-label="%s"></iframe>')
+                embed_code = video_url % (slide.video_url, 1600, 900, slide.video_title)
+                
+            slide.embed_code = embed_code
+            slide.embed_code_external = embed_code
